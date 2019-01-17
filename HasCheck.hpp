1cb346395932b30d042914cdaabd4e910c637ca8
ZMing <zming316@hotmail.com>
2019-01-17 14:46:40 +0800

    new file:   HasCheck.hpp

---
+++ HasCheck.hpp
@@ -0,0 +1,46 @@
+#pragma once
+#include <type_traits>
+
+template <typename ...>
+using void_t = void;
+
+template <typename T>
+using decay_t = typename std::decay<T>::type;
+
+#define HAS_STATIC_FUN(name ,func)                                                        \
+template <typename T, typename ...Args>                                                   \
+struct HasStaticMemberFunc##name                                                          \
+{                                                                                         \
+  template <typename U>                                                                   \
+  static auto                                                                             \
+    check(int) -> decltype(decay_t<U>::##func(std::declval<Args>()...), std::true_type()) \
+  {                                                                                       \
+    return std::bool_constant<true>();                                                    \
+  };                                                                                      \
+template <typename U>                                                                     \
+static auto                                                                               \
+  check(...) -> std::false_type                                                           \
+{                                                                                         \
+  return std::bool_constant<false>();                                                     \
+};                                                                                        \
+static constexpr bool value = std::is_same<decltype(check<T>(0)), std::true_type>::value; \
+};
+
+#define HAS_STATIC_FUN(name ,func)                                                        \
+template <typename T, typename ...Args>                                                   \
+struct HasMemberFunc##name                                                                \
+{                                                                                         \
+  template <typename U>                                                                   \
+  static auto                                                                             \
+    check(int) -> decltype(std::declval<U>().##func(std::declval<Args>()...), std::true_type()) \
+  {                                                                                       \
+    return std::bool_constant<true>();                                                    \
+  };                                                                                      \
+template <typename U>                                                                     \
+static auto                                                                               \
+  check(...) -> std::false_type                                                           \
+{                                                                                         \
+  return std::bool_constant<false>();                                                     \
+};                                                                                        \
+static constexpr bool value = std::is_same<decltype(check<T>(0)), std::true_type>::value; \
+};
